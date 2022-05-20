4924926d55b9bc8fde07d7a05db5d7f8c23b61d1
Monkreal <calvinscheidegger@outlook.com>
2022-05-20 08:57:39 +0000

    dhfsd

--- src/app/gui-model/guimodel.ts
+++ src/app/gui-model/guimodel.ts
@@ -6,7 +6,7 @@ export class GuiModel {
 
     private _guiModel = {
         "application": {
-            "title": "Calvin & Leandro Friend Tracker",
+            "title": "Georgios, Calvin & Leandro Friend Tracker",
             "formList": [
                 {
                     "id": "FriendForm",
